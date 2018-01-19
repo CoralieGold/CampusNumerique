7176fa0130670256db27ad317821a3f7f749160e
Coralie Goldbaum <coralie.gold@gmail.com>
2018-01-19 15:30:41 +0100

    Assistance contenu

--- assistance.php
+++ assistance.php
@@ -1,2 +1,84 @@
 <?php include_once("header.php") ?>
+
+<!-- CONTENT -->
+<main class="row">
+	<section class="lightSection col-12 container">
+		<h1 class="col-12 center-elements">Assistance</h1>
+		<div class="row justify-content-md-center">
+			<div class="btn-group" role="group" aria-label="FilterListButtons">
+				<button class="btn btn-primary col-auto filter-button" data-filter="all">Tout</button>
+				<button class="btn btn-primary col-auto filter-button" data-filter="cat1">Catégorie 1</button>
+				<button class="btn btn-primary col-auto filter-button" data-filter="cat2">Catégorie 2</button>
+				<button class="btn btn-primary col-auto filter-button" data-filter="cat3">Catégorie 3</button>
+			</div>
+		</div>
+		<br>
+
+		<div class="row">
+			<div class="card-group col-12">
+				<div class="col-3 filter gallery_product cat1">
+					<a class="card" href="reponse_assistance.php">
+						<i class="card-img-top background-fa-color fa fa-desktop fa-2x"></i>
+						<div class="card-body">
+							<h3>Ceci est une question de la page assistance ?</h3>
+						</div>
+						 <div class="card-footer text-muted">
+							Catégorie 1
+						</div>
+					</a>
+				</div>
+
+				<div class="col-3 filter gallery_product cat2">
+					<a class="card" href="reponse_assistance.php">
+						<i class="card-img-top background-fa-color fa fa-desktop fa-2x"></i>
+						<div class="card-body">
+							<h3>Ceci est une question de la page assistance ?</h3>
+						</div>
+						 <div class="card-footer text-muted">
+							Catégorie 2
+						</div>
+					</a>
+				</div>
+
+				<div class="col-3 filter gallery_product cat3">
+					<a class="card" href="reponse_assistance.php">
+						<i class="card-img-top background-fa-color fa fa-desktop fa-2x"></i>
+						<div class="card-body">
+							<h3>Ceci est une question de la page assistance ?</h3>
+						</div>
+						 <div class="card-footer text-muted">
+							Catégorie 3
+						</div>
+					</a>
+				</div>
+
+				<div class="col-3 filter gallery_product cat1">
+					<a class="card" href="reponse_assistance.php">
+						<i class="card-img-top background-fa-color fa fa-desktop fa-2x"></i>
+						<div class="card-body">
+							<h3>Ceci est une question de la page assistance ?</h3>
+						</div>
+						 <div class="card-footer text-muted">
+							Catégorie 1
+						</div>
+					</a>
+				</div>
+
+				<div class="col-3 filter gallery_product cat3">
+					<a class="card" href="reponse_assistance.php">
+						<i class="card-img-top background-fa-color fa fa-desktop fa-2x"></i>
+						<div class="card-body">
+							<h3>Ceci est une question de la page assistance ?</h3>
+						</div>
+						 <div class="card-footer text-muted">
+							Catégorie 3
+						</div>
+					</a>
+				</div>
+
+			</div>
+		</div>
+	</section>
+</main>
+
 <?php include_once("footer.php") ?>
--- reponse_assistance.php
+++ reponse_assistance.php
@@ -1,2 +1,43 @@
 <?php include_once("header.php") ?>
+
+<main class="row">
+	<section class="lightSection col-12">
+		<h1 class="col-12">Ceci est une question de la page assistance ?</h1>
+		<div class="col-12 text-center">
+			<img src="http://fakeimg.pl/350x150/?text=Illustration" class="col-6">
+		</div>
+		<div class="row justify-content-md-center">
+			<div class="col-10">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+		<div class="row justify-content-md-center">
+			<a class="btn btn-dark" href="assistance.php">Voir toutes les questions</a>
+		</div>
+	</section>
+</main>
+
 <?php include_once("footer.php") ?>
