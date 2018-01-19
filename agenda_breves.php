1dc86cb7e7f4ee2cbfdb69a8194bba07fd8b4046
Coralie Goldbaum <coralie.gold@gmail.com>
2018-01-19 17:48:32 +0100

    Agenda & Breves

--- agenda_breves.php
+++ agenda_breves.php
@@ -1,1 +1,98 @@
+<?php include_once("header.php") ?>
+
+<!-- CONTENT -->
+<main class="row">
+	<section class="darkSection col-12 container">
+		<h1 class="col-12 center-elements">Agenda</h1>
+		<div class="row">
+			<div class="card-deck col-12">
+				<div class="col-3">
+					<div class="card">
+						<img class="card-img-top" src="https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="evenement.html" class="btn btn-primary">Go somewhere</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="card">
+						<img class="card-img-top" src="https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="evenement.html" class="btn btn-primary">Go somewhere</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="card">
+						<img class="card-img-top" src="https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="evenement.html" class="btn btn-primary">Go somewhere</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="card">
+						<img class="card-img-top" src="https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="evenement.html" class="btn btn-primary">Go somewhere</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="card">
+						<img class="card-img-top" src="https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="evenement.html" class="btn btn-primary">Go somewhere</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="card">
+						<img class="card-img-top" src="https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="evenement.html" class="btn btn-primary">Go somewhere</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="card">
+						<img class="card-img-top" src="https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="evenement.html" class="btn btn-primary">Go somewhere</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="card">
+						<img class="card-img-top" src="https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="evenement.html" class="btn btn-primary">Go somewhere</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="lightSection col-12 container">
+		<h1 class="col-12 center-elements">Brèves</h1>
+	</section>
+</main>
+
 <?php include_once("footer.php") ?>
